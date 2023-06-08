d53a8f5f94e032e2c6cedc8bf9338f9617700988
NIKITAMELANIN <cool.melanin@list.ru>
2023-06-08 13:31:30 +0500

    front_main

--- Frontend/app/src/App.tsx
+++ Frontend/app/src/App.tsx
@@ -1,24 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const click = () => {
+};
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="service-information-welcome">Добро пожаловать <div className="new-line" >на платформу
+            <span
+            className="service-information-welcome-blue"> pro
+            </span>
+            ход!</div>
+        </div>
+        <div className="service-information-welcome-text">
+            Сервис создан для избавления гостей от бумажной работы <div className="new-line" >
+            и постоянных согласований, достаточно заполнить заявку на сайте</div> <div className="new-line" >
+            и ожидать одобрения в виде QR-кода на указанную почту. <div className="new-line" >
+            Пожалуйста, заполните все поля согласно инструкциям на сайте.</div>
+        </div>
+        <button type="button"  onClick={click} className="service-information-enter">пройти в
+            ирит-ртф
+        </button>
+        {/*<img className="service-information-cat" src="styles/cat-b.svg"/>*/}
+    </div>
+    {/*<svg className="rectangle" width="1450" height="700" transform="translate(50 1)">*/}
+    {/*    <line*/}
+    {/*        x1="20"*/}
+    {/*        y1="170"*/}
+    {/*        x2="1420"*/}
+    {/*        y2="170"*/}
+    {/*        // style="*/}
+    {/*        //         stroke: white;*/}
+    {/*        //         stroke-width: 2;"*/}
+    {/*    />*/}
+    {/*    <rect*/}
+    {/*        x="60"*/}
+    {/*        y="70"*/}
+    {/*        rx="35"*/}
+    {/*        ry="35"*/}
+    {/*        // style="*/}
+    {/*        //             width:1320px;*/}
+    {/*        //             height:600px;*/}
+    {/*        //             fill:none;*/}
+    {/*        //             stroke:white;*/}
+    {/*        //             stroke-width:2;*/}
+    {/*        //             opacity:1;"*/}
+
+    {/*    />*/}
+    {/*    <line*/}
+    {/*        x1="372"*/}
+    {/*        y1="565"*/}
+    {/*        x2="420"*/}
+    {/*        y2="565"*/}
+    {/*        // style="*/}
+    {/*        //         stroke: #6BC8F4;*/}
+    {/*        //         stroke-width: 2;"*/}
+    {/*    />*/}
+    {/*    <line*/}
+    {/*        x1="420"*/}
+    {/*        y1="565"*/}
+    {/*        x2="420"*/}
+    {/*        y2="617"*/}
+    {/*        // style="*/}
+    {/*        //         stroke: #6BC8F4;*/}
+    {/*        //         stroke-width: 2;"*/}
+    {/*    />*/}
+    {/*    <line*/}
+    {/*        x1="420"*/}
+    {/*        y1="617"*/}
+    {/*        x2="470"*/}
+    {/*        y2="617"*/}
+    {/*        // style="*/}
+    {/*        //         stroke: #6BC8F4;*/}
+    {/*        //         stroke-width: 2;"*/}
+    {/*    />*/}
+    {/*    <line*/}
+    {/*        x1="470"*/}
+    {/*        y1="617"*/}
+    {/*        x2="470"*/}
+    {/*        y2="667"*/}
+    {/*        // style="*/}
+    {/*        //         stroke: #6BC8F4;*/}
+    {/*        //         stroke-width: 2;"*/}
+    {/*    />*/}
+    {/*    </svg>*/}
+
     </div>
   );
 }
